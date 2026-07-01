656cbeccff2a62f1f62d727cae80b3051edb6f96
FrankHakim <frank.hakim@digitatanetworks.com>
2026-07-01 07:56:14 +0200

    Refactor Counter component to manage game state and status using a single state object

--- CountOpedia/src/Counter.jsx
+++ CountOpedia/src/Counter.jsx
@@ -3,32 +3,38 @@ import attackImage from './images/attack.png';
 import defendImage from './images/defend.png';
 
 function Counter() {
-    const [count, setCount] = useState(0);
-    const [gameStatus, setGameStatus] = useState("In Progress");
+
+    function getGameStatus(count) {
+        if (count >= 5) {
+            return "You Win!";
+        }
+
+        if (count <= -5) {
+            return "You Lose!";
+        }
+
+        return "In Progress";
+    }
+
+    const [gameState, setGameState] = useState({
+        count: 0,
+        gameStatus: "In Progress"
+    });
 
     function handleIncrement() {
-       const newValue = prevCount + 1;
-       setCount(newValue);
-       if(newValue >= 5) {
-        setGameStatus("You Win!");
-       }else{ if(newValue <= -5){
-        setGameStatus("You Lose!");
-       }
-       setCount(function(prevCount) {
-        return newValue;
-       });
+        setGameState((prevState) => ({
+            count: prevState.count + 1,
+            gameStatus: getGameStatus(prevState.count + 1)
+        }));
     }
+
     function handleDecrement() {
-       setCount(function(prevCount) {
-        const newValue = prevCount - 1;
-        if (newValue >= 5) {
-            setGameStatus("You Win!");
-        } else if (newValue <= -5) {
-            setGameStatus("You Lose!");
-        }
-        return newValue;
-       });
+        setGameState((prevState) => ({
+            count: prevState.count - 1,
+            gameStatus: getGameStatus(prevState.count - 1)
+        }));
     }
+
 
     function handleRandomPlay() {
         const randomNumber = Math.floor(Math.random() * 2);
@@ -40,53 +46,57 @@ function Counter() {
     }
 
     function handleReset() {
-        setCount(0);
+        setGameState({
+            count: 0,
+            gameStatus: getGameStatus(0)
+        });
     }
 
     function handleLog() {
-        console.log("Current count:", count);
+        console.log("Current count:", gameState.count);
     }
 
     return (  
         <div className="container">
             <div className="row text-white text-center container">
-                <h1 className="h1 m-3">Game Score : {count}</h1>
+                <h1 className="h1 m-3">Game Score : {gameState.count}</h1>
                 <p>You win at +5 points and lose at -5 points!</p>
-                <h3 >Game Status: {gameStatus}</h3>
+                {gameState.gameStatus.length > 0 && <h3 >Game Status: {gameState.gameStatus}</h3>}
 
-                <div className = "col-6 col-md-3 offset-md-3">
-                    <img src={attackImage} style={
-                        {
-                            width: "100%",
-                            cursor: "pointer",
-                            border: "1px solid green"
-                        }} 
-                        onClick={handleIncrement}
-                        className="mb-3"
-                        
-                    />
-                </div>
+                <div className="col-12 col-md-8 mx-auto">
+                    <div className="row g-3">
+                        <div className="col-6">
+                            <img
+                                src={attackImage}
+                                style={{
+                                    width: "100%",
+                                    cursor: "pointer",
+                                    border: "1px solid green"
+                                }}
+                                onClick={handleIncrement}
+                                className="mb-3"
+                            />
+                        </div>
 
-                <div className = "col-6 col-md-3 offset-md-3">
-                    <img src={defendImage} 
-                    style={{
-                            width: "100%",
-                            cursor: "pointer",
-                            border: "1px solid red"
-                        }} 
-                        onClick={handleDecrement}
-                        className="p-4 rounded mb-3">
-                        
-                    </img>
+                        <div className="col-6">
+                            <img
+                                src={defendImage}
+                                style={{
+                                    width: "100%",
+                                    cursor: "pointer",
+                                    border: "1px solid red"
+                                }}
+                                onClick={handleDecrement}
+                                className="p-4 rounded mb-3"
+                            />
+                        </div>
+                    </div>
 
-                </div>
-
-                <div className="col-12 col-md-4 offset-md-3">
-                         <button className="btn btn-success w-100" onClick={handleRandomPlay}>Random Play</button>
-                        <br />
-                        <button  className="btn btn-danger w-100" onClick={handleReset}>Reset</button>
-                       <br />
-                        <button  className="btn btn-warning w-100" onClick={handleLog}>Log</button>
+                    <div className="d-grid gap-2 mt-2">
+                        <button className="btn btn-success" onClick={handleRandomPlay}>Random Play</button>
+                        <button className="btn btn-danger" onClick={handleReset}>Reset</button>
+                        <button className="btn btn-warning" onClick={handleLog}>Log</button>
+                    </div>
                 </div>
 
 
